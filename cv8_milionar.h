f3d37efa461bce93923e7b1e008cbf8f08c182cf
Tomas Homola <homola.tom6@gmail.com>
2020-11-19 18:31:17 +0100

    Vytvorene GUI, pridane deklaracie funkcii pre jednotlive tlacitka

--- cv8_milionar.h
+++ cv8_milionar.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
-#include "ui_cv8_milionar.h"
+#include <ui_cv8_milionar.h>
 
 class cv8_milionar : public QMainWindow
 {
@@ -12,4 +12,14 @@ public:
 
 private:
     Ui::cv8_milionarClass ui;
+
+private slots:
+    void on_pushButtonNewGame_clicked(); // nova hra
+    void on_pushButtonEndGame_clicked(); // koniec hry
+    void on_pushButtonAccept_clicked(); // potvrdenie vyberu
+    void on_pushButtonSkip_clicked(); // preskocenie otazky
+
+    void on_pushButtonZolik1(); // zolik 1
+    void on_pushButtonZolik2(); // zolik 2
+    void on_pushButtonZolik3(); // zolik 3
 };
--- source_cv8_milionar.cpp
+++ source_cv8_milionar.cpp
@@ -1,7 +1,49 @@
 #include "cv8_milionar.h"
 
-cv8_milionar::cv8_milionar(QWidget *parent)
-    : QMainWindow(parent)
+cv8_milionar::cv8_milionar(QWidget *parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    // deaktivacia buttonov, ktore sa nebudu dat stlacit, kym sa nezacne hra
+    ui.pushButtonEndGame->setEnabled(false);
+    ui.pushButtonAccept->setEnabled(false);
+    ui.pushButtonSkip->setEnabled(false);
+    ui.pushButtonZolik1->setEnabled(false);
+    ui.pushButtonZolik2->setEnabled(false);
+    ui.pushButtonZolik3->setEnabled(false);
+}
+
+void cv8_milionar::on_pushButtonNewGame_clicked() // button Nova hra
+{
+
+}
+
+void cv8_milionar::on_pushButtonEndGame_clicked() // button Ukoncit hru
+{
+
+}
+
+void cv8_milionar::on_pushButtonAccept_clicked() // button Potvrdit
+{
+
+}
+
+void cv8_milionar::on_pushButtonSkip_clicked() // button Preskocit otazku
+{
+
+}
+
+void cv8_milionar::on_pushButtonZolik1() // zolik 1
+{
+
+}
+
+void cv8_milionar::on_pushButtonZolik2() // zolik 2
+{
+
+}
+
+void cv8_milionar::on_pushButtonZolik3() // zolik 3
+{
+
 }
