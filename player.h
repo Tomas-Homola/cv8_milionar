0808e43deaac540416a17bb64a2af36bafba71bc
Tomas Homola <homola.tom6@gmail.com>
2020-11-21 18:32:50 +0100

    Zmena typu std::string na QString pre premennu playerName

--- player.h
+++ player.h
@@ -1,1 +1,19 @@
 #pragma once
+
+class Player
+{
+private:
+	QString playerName = "";
+	double playerScore = -1.0;
+public:
+	Player() {}
+	Player(QString playerName, double playerScore) { this->playerName = playerName; this->playerScore = playerScore; }
+	
+	void setPlayerName(QString newPlayerName) { playerName = newPlayerName; }
+	void setPlayerScore(double newPlayerScore) { playerScore = newPlayerScore; }
+	
+	QString getPlayerName() { return playerName; }
+	double getPlayerScore() { return playerScore; }
+
+	
+};
